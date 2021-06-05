e3fd655afede7533c62458b1f5ac8c4d6d55c928
krigor-cintra <krigor.gcs@gmail.com>
2021-06-05 20:53:35 -0300

    Update App.java
    Atualização

--- src/App.java
+++ src/App.java
@@ -6,12 +6,12 @@ public class App {
         
         Controle controle = new Controle();
 
-        //controle.cad_aluno(); // cria o cadastro ( do telefone também) e joga no banco de dados
-       // controle.cad_telefone(); // cria os telefones da criaturas 
+        controle.cad_aluno(); // cria o cadastro ( do telefone também) e joga no banco de dados
+       //controle.cad_telefone(); // cria os telefones da criaturas - se não tiver colocado quando criou o demonio ( vai pedir o id da criatura)
 
         //controle.apagar_aluno(); //apaga tudo
 
-       // controle.dados_aluno();//mostra os dados do aluno(id-nome-cidade-estado-telefone-idtelefone)
+        //controle.dados_aluno();//mostra os dados do aluno(id-nome-cidade-estado-telefone-idtelefone)
         //controle.ultimo_id();
        //controle.atualizar_aluno();//atualiza
 
